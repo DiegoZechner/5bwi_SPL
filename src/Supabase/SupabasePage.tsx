2f2839480686e37773e28b3d3388265a6a54fcc0
Diego <you@example.com>
2025-01-26 18:17:43 +0100

    Add Homework

--- src/Supabase/SupabasePage.tsx
+++ src/Supabase/SupabasePage.tsx
@@ -1,24 +1,23 @@
-import { useState, useEffect } from 'react';
-import supabase from './supabaseClient';
-import { data } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import supabase from "./supabaseClient";
+import { data } from "react-router-dom";
 
 export default function SupabasePage() {
   const [homework, setHomework] = useState([]);
-  const [subject, setSubject] = useState('');
-  const [note, setNote] = useState('');
+  const [subject, setSubject] = useState("");
+  const [note, setNote] = useState("");
 
   useEffect(() => {
     async function fetchHomework() {
       const { data } = await supabase
-        .from('homework')
-        .select('*')
-        .order('id', { ascending: true });
+        .from("homework")
+        .select("*")
+        .order("id", { ascending: true });
     }
-
 
     fetchHomework();
   }, []);
-
-
-
+  const handleAddHomework = async () => {
+    const {} = await supabase.from("homework").insert([{ subject, note }]);
+  };
 }
